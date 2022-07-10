69e6e48cb3ef0d5c5f2cd5613235ee4e6476d38c
BobThBuilder_74 <94049554+AmanGhosh74@users.noreply.github.com>
2022-07-10 19:43:05 +0530

    Update 3. Number of Island.cpp

--- Graph/Leetcode/Matrix Related problems/3. Number of Island.cpp
+++ Graph/Leetcode/Matrix Related problems/3. Number of Island.cpp
@@ -1,10 +1,9 @@
-// 3. Number of islands
-/*
+/* 3. Number of islands
 Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all
 surrounded by water.
 */
-// DFS
+//-------->  DFS Solution
 class Solution
 {
 public:
@@ -56,7 +55,7 @@ public:
     }
 };
 
-// BFS
+// ---------> BFS solution
 class Solution
 {
 public:
@@ -114,3 +113,6 @@ public:
         return island;
     }
 };
+
+
+// Time and space complexity for both algo is O(m*n)
