8a743e3eb284d7fca869725ef3f6c5cb7bb930a6
BobThBuilder_74 <94049554+AmanGhosh74@users.noreply.github.com>
2022-07-10 19:47:16 +0530

    Update 4. Maximum Area.cpp

--- Graph/Leetcode/Matrix Related problems/4. Maximum Area.cpp
+++ Graph/Leetcode/Matrix Related problems/4. Maximum Area.cpp
@@ -1,4 +1,8 @@
-// 4. Max area
+/*695. Max Area of Island
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume 
+all four edges of the grid are surrounded by water. The area of an island is the number of cells with a value 1 in the island.
+Return the maximum area of an island in grid. If there is no island, return 0.
+*/
 class Solution
 {
 public:
@@ -12,12 +16,14 @@ public:
         {
             return 0;
         }
-        // if it is not visited
-
+        // if it is not visited change land to water (acting as a visited array)
+        
         vis[row][col] = 1;
+        
+        // initial area
         int area = 0;
 
-        // go to all pos and increase cnt
+        // go to all 4 directions  and increase area every time it is valid 
         area = 1 + dfs(row - 1, col, grid, vis) + dfs(row, col - 1, grid, vis) + dfs(row + 1, col, grid, vis) + dfs(row, col + 1, grid, vis);
 
         return area;
@@ -31,6 +37,7 @@ public:
             for (int j = 0; j < grid[0].size(); j++)
             {
                 int area = 0;
+                // it is already visited if it is water 
                 if (grid[i][j] == 1)
                     area = dfs(i, j, grid, vis);
                 ans = max(ans, area);
