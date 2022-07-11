43d80194b7c7de3384a83824b9133ae9c69f5903
BobThBuilder_74 <94049554+AmanGhosh74@users.noreply.github.com>
2022-07-11 16:55:57 +0530

    Update A.Cakeminator.cpp

--- Implementation/A.Cakeminator.cpp
+++ Implementation/A.Cakeminator.cpp
@@ -1,15 +1,22 @@
 // A. Cakeminator --> https://codeforces.com/problemset/problem/330/A
+
+// By observation we can see that apples which has both row and col as evil stawberies cant be eaten so we created 1d array of row and coloumn and increase it whenever 
+// we came across a evil stawberies
 int row[11], col[11];
 void solve()
 {
     int r, c;
     cin >> r >> c;
     char arr[r][c];
+    
+    // input matrix
     for (int i = 0; i < r; i++)
     {
         for (int j = 0; j < c; j++)
         {
             cin >> arr[i][j];
+            
+            // if there is a evil stawberry we initialize both row and coln of that i&j
             if (arr[i][j] == 'S')
             {
                 row[i] = 1;
@@ -17,17 +24,22 @@ void solve()
             }
         }
     }
+    // eating cake 
     int cake = 0;
     for (int i = 0; i < r; i++)
     {
         for (int j = 0; j < c; j++)
-        {
+        {   
+            
+            // whenever there is S or grid with evil on both row and coln of that grid then we dont eat that cake 
             if (arr[i][j] == 'S')
                 continue;
             if (row[i] && col[j])
             {
                 continue;
             }
+            
+            //else eat that cake 
             cake++;
         }
     }
