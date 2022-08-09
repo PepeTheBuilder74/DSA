7fc0d3ff6a530556de7668cc842db08699f0fd0c
BobThBuilder_74 <94049554+AmanGhosh74@users.noreply.github.com>
2022-08-09 18:55:19 +0530

    Add files via upload

---
+++ DP/Kadane's Algorithm'/2_Max_Product_Subarray.cpp
@@ -0,0 +1,38 @@
+/*
+152. Maximum Product Subarray
+Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+The test cases are generated so that the answer will fit in a 32-bit integer.
+A subarray is a contiguous subsequence of the array.
+*/
+
+/*2. Maximun Product Subarray
+
+TC->
+SC->
+ASC->
+
+*/
+int maxProduct(int A[], int n)
+{
+    // store the result that is the max we have found so far
+    int r = A[0];
+
+    // imax/imin stores the max/min product of
+    // subarray that ends with the current number A[i]
+    for (int i = 1, imax = r, imin = r; i < n; i++)
+    {
+        // multiplied by a negative makes big number smaller, small number bigger
+        // so we redefine the extremums by swapping them
+        if (A[i] < 0)
+            swap(imax, imin);
+
+        // max/min product for the current number is either the current number itself
+        // or the max/min by the previous number times the current one
+        imax = max(A[i], imax * A[i]);
+        imin = min(A[i], imin * A[i]);
+
+        // the newly computed max value is a candidate for our global result
+        r = max(r, imax);
+    }
+    return r;
+}
