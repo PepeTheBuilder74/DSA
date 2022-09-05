d16cd226f256018f5ec891b7dbf14cfad9fde7a6
Aman Ghosh <94049554+BobTheBuilder74@users.noreply.github.com>
2022-09-06 00:02:58 +0530

    Add files via upload

---
+++ Bits/Generate_Subsets.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> subsets(vector<int> &nums)
+{
+
+    vector<vector<int>> ans;
+    int n = nums.size();
+    int susbset_ct = 1 << n;
+    for (int mask = 0; i < susbset_ct; i++)
+    {
+        vector<int> temp;
+        for (int j = 0; j < n; j++)
+        {
+            if (mask & (1 << j))
+            {
+                temp.push_back(nums[j]);
+            }
+        }
+        ans.push_back(temp);
+    }
+    return ans;
+}
+
+// time complexity: O(2^n * n)
+// space complexity: O(2^n * n)
+// auxillary space: O(n)
