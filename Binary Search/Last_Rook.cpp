5f5cfa133b56a46baff5f0db883a6e8a50dc9ad3
Aman Ghosh <94049554+PepeTheBuilder74@users.noreply.github.com>
2022-09-19 01:35:09 +0530

    Add files via upload

---
+++ Binary Search/Last_Rook.cpp
@@ -0,0 +1,74 @@
+// E - Last Rook
+
+#include <bits/stdc++.h>
+using namespace std;
+const int mod = 1e9 + 7;
+#define ll long long int
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define ar array
+const int MxN = 2e5;
+ar<int, 2> v[MxN];
+
+int main()
+{
+    int n;
+    cin >> n;
+    int low = 1, high = n;
+    int r = -1, c = -1;
+
+    // for finding column by keeping row fixed
+    // if high - mid+ 1 == x then we have to move to serach in left half because there are already x number
+    // of rooks in right half
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        cout << "? " << 1 << " " << n << " " << mid << " " << high << endl;
+        int x;
+        cin >> x;
+        if (x == 0)
+        {
+            c = mid;
+            break;
+        }
+
+        if (x == high - mid + 1)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            c = mid;
+            low = mid + 1;
+        }
+    }
+
+    // for finding row by keeping column fixed
+    // if high - mid+ 1 == x then we have to move to serach in left half because there are already x number
+    // of rooks in right half
+    low = 1, high = n;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        cout << "? " << mid << " " << high << " " << 1 << " " << n << endl;
+        int x;
+        cin >> x;
+        if (x == 0)
+        {
+            r = mid;
+            break;
+        }
+
+        if (x == high - mid + 1)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            r = mid;
+            low = mid + 1;
+        }
+    }
+
+    cout << "! " << r << " " << c << endl;
+}
