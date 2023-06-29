8ae9c2e4497b2bf0c183520e30e5dcdf92d75ff5
Aman Ghosh <94049554+PepeTheBuilder74@users.noreply.github.com>
2023-06-29 19:05:18 +0530

    Update SegmentTree.cpp

--- AlgoLibrary/RangeQueries/SegmentTree.cpp
+++ AlgoLibrary/RangeQueries/SegmentTree.cpp
@@ -1,56 +1,76 @@
-#define ll int64_t;
-struct node {
+struct node
+{
     int val;
-    node(ll x) {
-        val=0;
+    node(int x)
+    {
+        val = x;
     }
-
-    node() {
-        val=0;
+    // default value
+    node()
+    {
+        val = 0;
     }
 };
-template<typename T>
-class segtree {
+template <typename T>
+class segtree
+{
 public:
-// 0 based indexing
-// def= default value
-	vector<T> t;
-	int n;
-	T def;
-	function<T(T, T)>merge;
-	void build(int _n, T _def, function<T(T, T)> _fx) {
-		n = _n; def = _def;merge = _fx;
-		t.assign(n * 2, def);
-		for(int i = n - 1; i; i--) t[i] =merge(t[i * 2], t[i * 2 + 1]);
-	}
-	void build(const vector<ll>& a, T _def, function<T(T, T)> _fx) {
-		n = a.size(); def = _def;merge = _fx;
-		t.assign(n * 2, def);
-		for(int i=0;i<n;i++) t[i + n] = T(a[i]);
-		for(int i = n - 1; i; i--) t[i] =merge(t[i * 2], t[i * 2 + 1]);
-	}
-	void update(int i, T v) {
-		for(t[i += n] = T(v); i > 1; ) {
-			i /= 2;
-			t[i] =merge(t[i * 2], t[i * 2 + 1]);
-		}
-	}
-	// this query is made on [l, r)
-	T query(int l, int r) {
-		T lans = def, rans = def;
-		for(l += n, r += n; l < r; l /= 2, r /= 2) {
-			if(l % 2) lans =merge(lans, t[l++]);
-			if(r % 2) rans =merge(t[--r], rans);
-		}
-		return merge(lans, rans);
-	}
-	void debug() {
-        cout<<"[ ";
-		each(x, t) cout << x << " , ";
-        cout<<"]";
+    // 0 based indexing
+    // def= default value
+    vector<T> t;
+    int n;
+    T def;
+    function<T(T, T)> merge;
+    void build(int _n, T _def, function<T(T, T)> _fx)
+    {
+        n = _n;
+        def = _def;
+        merge = _fx;
+        t.assign(n * 2, def);
+        for (int i = n - 1; i; i--)
+            t[i] = merge(t[i * 2], t[i * 2 + 1]);
+    }
+    void build(vector<int> &a, T _def, function<T(T, T)> _fx)
+    {
+        n = a.size();
+        def = _def;
+        merge = _fx;
+        t.assign(n * 2, def);
+        for (int i = 0; i < n; i++)
+            t[i + n] = T(a[i]);
+        for (int i = n - 1; i; i--)
+            t[i] = merge(t[i * 2], t[i * 2 + 1]);
+    }
+    void update(int i, int v)
+    {
+        for (t[i += n] = T(v); i > 1;)
+        {
+            i /= 2;
+            t[i] = merge(t[i * 2], t[i * 2 + 1]);
+        }
+    }
+    // this query is made on [l, r)
+    T query(int l, int r)
+    {
+        T lans = def, rans = def;
+        for (l += n, r += n; l < r; l /= 2, r /= 2)
+        {
+            if (l % 2)
+                lans = merge(lans, t[l++]);
+            if (r % 2)
+                rans = merge(t[--r], rans);
+        }
+        return merge(lans, rans);
+    }
+    void debug()
+    {
+        cout << "[ ";
+        each(x, t) cout << x << " , ";
+        cout << "]";
         cout << endl;
-	}
+    }
 };
+
 int main(){
    auto merge=[](node a,node b){
        node c;
